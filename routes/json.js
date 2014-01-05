615601bd948a3ae47413a0293385194b0215f5d1
Hideki Yamamura <hideki.yamamura@gmail.com>
2014-01-05 22:48:19 +0900

    avoid a repeated variable declaration

--- routes/json.js
+++ routes/json.js
@@ -1,10 +1,11 @@
 exports.test = function(req, res) {
     var type = req.query.type;
     res.contentType('application/json');
+    var data;
     if (type === 'hoge') {
-        var data = { name: 'hoge', type: type };
+        data = { name: 'hoge', type: type };
     } else {
-        var data = { name: 'shichiku', blood: 'B', weight: '65kg' };
+        data = { name: 'shichiku', blood: 'B', weight: '65kg' };
     }
     res.send(JSON.stringify(data));
 }
